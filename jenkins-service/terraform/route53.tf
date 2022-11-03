07d51bdf89bcb8ff76fdb98a01a2275fee39e936
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-11-03 18:46:27 +0800

    chore(jenkins-service): add certificate to jenkins domain

--- jenkins-service/terraform/route53.tf
+++ jenkins-service/terraform/route53.tf
@@ -25,3 +25,20 @@ resource "aws_route53_record" "jenkins-www" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "jenkins-cert" {
+  for_each = {
+    for dvo in aws_acm_certificate.jenkins.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.jenkins.zone_id
+}
