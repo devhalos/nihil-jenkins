ebaa78d348ea324297d396bd94b09139a3da6275
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2022-11-08 22:23:28 +0800

    chore(jenkins-app): encrypt ecr repository with kms

--- jenkins-app/terraform/ecr.tf
+++ jenkins-app/terraform/ecr.tf
@@ -5,4 +5,9 @@ resource "aws_ecr_repository" "jenkins" {
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = aws_kms_key.jenkins_ecr.key_id
+  }
 }
